94b7d43de1370ad97942e960c89cba2f5dead1cd
Alexander Mayo <admin@freelabel.net>
2015-12-23 12:28:31 -0600

    files now show a dropdown menu for adding files to already existing promotions

--- users/application/views/dashboard/attach.php
+++ users/application/views/dashboard/attach.php
@@ -9,10 +9,10 @@ $user_promos = $config->get_user_promos(Session::get('user_name'));
 // $params[] = array('value'=>'user_name', 'text'=>'user_name','label'=>'Choose Username..');
 
 ?>
-<form class='attach_file_form'>
+<form method="post" class='attach_file_form'>
 	<label>Choose a Promotion..</label>
 	<?php echo $config->build_dropdown($user_promos); ?>
-	<button class="btn-success btn-block add-promo-button" >Add</button>
+	<button class="btn-success btn-block" >Add</button>
 </form>
 
 
@@ -22,9 +22,15 @@ $user_promos = $config->get_user_promos(Session::get('user_name'));
 
 <script type="text/javascript">
 	$(function(){
-		$('.add-promo-button').click(function(){
-			var data = $(this).parent().serializeArray();
-			alert(data);
+
+		$('.attach_file_form').submit(function(event){
+			event.preventDefault();
+			var data = $(this).find('select').val();
+			var wrapper = $(this);
+			wrapper.text('Successfully Pushed!');
+			setTimeout(function(){
+				wrapper.hide('fast');
+			},3000);
 		});
 	});
 </script>
