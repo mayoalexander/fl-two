da0b264ffb1dcfffceb1488e74ac76072b1f369a
Alexander Mayo <admin@freelabel.net>
2016-02-22 15:20:30 -0600

    promos update

--- users/application/views/index/promos.php
+++ users/application/views/index/promos.php
@@ -21,7 +21,7 @@
 
 
 
-<section class="col-md-8" >
+<section >
 <?php
   include_once('/home/content/59/13071759/html/config/index.php');
   $config = new Blog();
@@ -33,134 +33,39 @@
   echo $config->display_promo_public($promos, true); 
 ?>
 </section>
-<section class="col-md-4">
-<h1>hello gys</h1>
-</section>
 </div>
 
-<script type="text/javascript">
-	$(function(){
-		// config
-	  	function isPlaying(audelem) {
-	    	return !audelem.paused;
-	  	}
-
-		// Custom Controls
-	    var globalAudioPlayer = $(".audio-player");
-	    var globalButtons = $(".promo-file-options .attached-file-button");
-	    var globalAudioPlayerText =  $(".audio-player-title");
-
-		// $('.promo-file-options').click(function(event){
-		// 	event.preventDefault();
-		// 	alert($(this).attr('data-url'));
-		// });
 
 
 
 
 
 
-    // ********************************* 
-    //  PLAY BUTTON CONTROL 
-    // *********************************
-
-    //  ---------- play button ------------ /
-    $('.promo-file-options').click(function(event){
-    // console.log(globalButtons);
-      event.preventDefault();
-      var audioFile = $(this).attr('data-src');
-      var audioTitle = $(this).attr('data-title');
-      var nowplaying = '<i class="fa fa-play"></i>';
-      var nowpaused = '<i class="fa fa-pause"></i>';
-      // get next song
-      var nextsong = $(this).parent().parent().next();
-      var nextFile = nextsong.find('.controls-play').attr('data-src');
-      var nextTitle = nextsong.find('.controls-play').attr('data-title');
-      globalButtons.removeClass('fa fa-pause'); // * 
-      globalButtons.addClass('fa fa-play'); // * 
-      
-
-
-
-      // console.log(nextFile);
-      // console.log(nextsong);
-      // console.log(audioFile);
-      // console.log(globalAudioPlayer[0].src);
-
-
-      if (isPlaying(globalAudioPlayer[0])==false) {
-        // play file
-        	// console.log();
-        	// var trackListing = ;
-        	// alert(trackListing);
-        	// console.log(trackListing[0]);
-        	$(this).find('a').removeClass('fa-play-circle')
-        	$(this).find('a').addClass('fa-pause');
-        	// trackListing[0].removeClass('fa-play-circle');
-        	// trackListing[0].addClass('fa-pause-circle');
-              // $(this).html('<i class="fa fa-pause"></i>');
-              globalAudioPlayer[0].play();
-              globalAudioPlayerText.text(audioTitle);
-              globalAudioPlayer.attr('src', audioFile);
-              globalAudioPlayer.attr('autoplay', 1);
-              $(this).addClass('now-playing'); // *
-              globalAudioPlayer.attr('loop', 1);
-      } else if (isPlaying(globalAudioPlayer[0])==true && audioFile !== globalAudioPlayer[0].src) {
-        // pause function
-            $(this).find('a').removeClass('fa-play-circle')
-        	$(this).find('a').addClass('fa-pause');
-              globalAudioPlayer[0].play();
-              globalAudioPlayerText.text(audioTitle);
-              globalAudioPlayer.attr('src', audioFile);
-              globalAudioPlayer.attr('autoplay', 1);
-              // globalAudioPlayer.attr('loop', 1);
-      } else {
-        $(this).html('<i class="fa fa-play"></i>');
-        globalAudioPlayer[0].pause();
-      }
-
-      if ($(this).html()==nowpaused) {
-          // alert('show pawuse : ' + $(this).html());
-          $(this).removeClass('btn-secondary-outline');
-          $(this).addClass('btn-primary-outline');
-      } else {
-          // alert('show play button ' + $(this).html());
-          // $(this).html('<i class="fa fa-pause"></i>');
-          $(this).removeClass('btn-secondary-outline');
-          $(this).addClass('btn-primary-outline');
-      }
-
-
-    });
 
 
 
 
 
- $('.share-promo-file').click(function(){
-  var promo = $('.promo-title').text();
-  var data = $(this).attr('data-id');
-  var title = $(this).attr('data-title');
-  var url      = window.location.href; 
-  var text = promo+": " + title;
 
-  // console.log(text);
-  var newURL = 'http://twitter.com/intent/tweet/?text='+ encodeURIComponent(text) + '&url=' + url;
-  window.open(newURL);
-  // alert(newURL);
-
-
-
- });
-
-
-
-
-
-	});
+<!-- Modal -->
+<div class="modal fade" id="promoModal" tabindex="-1" role="dialog" aria-labelledby="promoModalLabel">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+<!--       <div class="modal-header">
+        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+      </div> -->
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
 
 
+<script type="text/javascript" src="http://freelabel.net/js/promos.js"></script>
 
 
-
-</script>
