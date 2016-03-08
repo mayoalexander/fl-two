cb6e2fb6d7de8f720143bedb4435d883f2677615
Alexander Mayo <admin@freelabel.net>
2016-03-08 14:21:51 -0600

    live search

---
+++ landing/livesearch/index.php
@@ -0,0 +1,92 @@
+<?php
+
+$content ='<script type="text/javascript" src="jquery-1.8.0.min.js"></script>
+<script type="text/javascript">
+$(function(){
+$(".search").keyup(function() 
+{ 
+var searchid = $(this).val();
+var dataString = \'search=\'+ searchid;
+if(searchid!=\'\')
+{
+    $.ajax({
+    type: "POST",
+    url: "search.php",
+    data: dataString,
+    cache: false,
+    success: function(html)
+    {
+    $("#result").html(html).show();
+    }
+    });
+}return false;    
+});
+
+jQuery("#result").live("click",function(e){ 
+    var $clicked = $(e.target);
+    var $name = $clicked.find(\'.name\').html();
+    var decoded = $("<div/>").html($name).text();
+    $(\'#searchid\').val(decoded);
+});
+jQuery(document).live("click", function(e) { 
+    var $clicked = $(e.target);
+    if (! $clicked.hasClass("search")){
+    jQuery("#result").fadeOut(); 
+    }
+});
+$(\'#searchid\').click(function(){
+    jQuery("#result").fadeIn();
+});
+});
+</script>
+
+<style type="text/css">
+    .content{
+        width:900px;
+        margin:0 auto;
+    }
+    #searchid
+    {
+        width:500px;
+        border:solid 1px #000;
+        padding:10px;
+        font-size:14px;
+    }
+    #result
+    {
+        position:absolute;
+        width:500px;
+        padding:10px;
+        display:none;
+        margin-top:-1px;
+        border-top:0px;
+        overflow:hidden;
+        border:1px #CCC solid;
+        background-color: white;
+    }
+    .show
+    {
+        padding:10px; 
+        border-bottom:1px #999 dashed;
+        font-size:15px; 
+        height:50px;
+    }
+    .show:hover
+    {
+        background:#4c66a4;
+        color:#FFF;
+        cursor:pointer;
+    }
+</style>
+
+<div class="content">
+<input type="text" class="search" id="searchid" placeholder="Search for people" />&nbsp; &nbsp; Ex: <b><i>huzoor bux, neelam, ravi or sameer</i></b><br /> 
+<div id="result"></div>
+</div>';
+
+
+$pre = 1;
+$title = "How to Integrate live search in PHP and MySQL with jQuery";
+$heading = "How to Integrate live search in PHP and MySQL with jQuery example.";
+include("html.inc");
+?>
