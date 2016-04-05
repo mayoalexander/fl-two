32b52ac692eca6ae5289f54b8062650745257949
Alexander Mayo <admin@freelabel.net>
2016-04-05 10:12:07 -0500

    added style consistency within register/login views

--- users/application/views/login/create.php
+++ users/application/views/login/create.php
@@ -1,35 +1,10 @@
-
-
-<header class="jumbotron bg-inverse text-center center-vertically show-after-cta" role="banner">
-  <div class="container">
-    <h1 class="display-3">CREATE ACCOUNT</h1>
-
-
-
-    <h2 class="m-b-lg"><?php echo $site['description']; ?> <a href="" class="jumbolink">Join now</a>.</h2>
-    <!-- <a href="<?php echo $this->facebook_login_url; ?>" class="btn btn-secondary-outline m-b-md btn-facebook facebook-login-button"><i class="fa fa-facebook" ></i> Signin with Facebook</a> -->
-    <a href="<?php echo $this->facebook_register_url; ?>" class="btn btn-secondary-outline m-b-md btn-facebook facebook-login-button"><i class="fa fa-facebook" ></i> Register with Facebook</a>
-    <a class="btn btn-secondary-outline m-b-md" href="<?php echo $site['http']; ?>users/login#" role="button"><span class="icon-sketch"></span>Already Registered? Login Here</a>
-    <ul class="list-inline social-share">
-      <li><a class="nav-link" href="http://twitter.com/<?php echo $site['twitter']; ?>#"><span class="icon-twitter"></span> <?php echo $site['landing-info']['twitter']; ?></a></li>
-      <!-- <li><a class="nav-link" href="https://www.facebook.com/theAMRecords/#"><span class="icon-facebook"></span> <?php echo $site['landing-info']['facebook']; ?></a></li> -->
-      <!-- <li><a class="nav-link" href="#"><span class="icon-linkedin"></span> <?php //echo $site['landing-info']['twitter']; ?></a></li> -->
-    </ul>
-  </div>
-</header>
-
-
 <a name="register-form"></a>
-<header class="jumbotron bg-inverse text-center center-vertically show-after-cta" role="banner" style='background-image:none;'>
-        <link rel="stylesheet" type="text/css" href="http://freelabel.net/landing/view/inputs/css/normalize.css" />
-        <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.2.0/css/font-awesome.min.css" />
-        <!-- <link rel="stylesheet" type="text/css" href="http://freelabel.net/landing/view/inputs/css/demo.css" /> -->
-        <link rel="stylesheet" type="text/css" href="http://freelabel.net/landing/view/inputs/css/set1.css" />
+<header class="jumbotron bg-inverse text-center center-vertically show-after-cta" role="banner" style='background-image:none;background-color:transparent;'>
         <div class="container">
-            <section class="">
+
                 <form action="<?php echo URL; ?>login/register_action" method="POST" style="max-width:400px;margin:auto;" name="registerform" >
 
-                    <h1>Register</h1>
+                    <h1 class="display-3">REGISTER</h1>
                     <?php $this->renderFeedbackMessages(); ?>
 
                     <?php
@@ -51,17 +26,10 @@
                         $params_1[] = array('name'=>'captcha', 'id'=>'captcha','label'=>'Enter Captcha..');
                         echo $config->build_input($params_1);
                     ?>
-                    <span class="input input--akira">
-                      <input name="register" class="input__field input__field--akira" type="submit" value="Register" id="user_name">
-                      <label class="input__label input__label--akira" for="input-22">
-                          <span class="input__label-content input__label-content--akira"></span>
-                      </label>
-                    </span>
-
+                    <input name="register" class="btn btn-success-outline btn-block" type="submit" value="Register" id="user_name">
                     <a href="<?php echo URL; ?>login/register">Register</a> | <a href="<?php echo URL; ?>login/requestpasswordreset">Forgot my Password</a>
 
                 </form>
-            </section>
         </div><!-- /container -->
         <script src="http://freelabel.net/landing/view/inputs/js/classie.js"></script>
         <script>
